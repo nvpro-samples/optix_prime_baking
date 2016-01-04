c4160773fcaa35f0c9de9978c344251b725a0c4e
Dylan Lacewell <lacewell@gmail.com>
2016-01-04 14:54:39 -0700

    fix timing code on Windows

--- bake_util.cpp
+++ bake_util.cpp
@@ -25,23 +25,74 @@
 #include <iomanip>
 #include <iostream>
 
+// System timing code copied from OptiX SDK
+#if defined(_WIN32)
+#    define WIN32_LEAN_AND_MEAN
+#    include<windows.h>
+#    include<mmsystem.h>
+#else /*Apple and Linux both use this */
+#    include<sys/time.h>
+#    include <unistd.h>
+#    include <dirent.h>
+#endif
+
+#if defined(_WIN32)
+
+// inv_freq is 1 over the number of ticks per second.
+static double inv_freq;
+static int freq_initialized = 0;
+static int use_high_res_timer = 0;
+
+bool sutilCurrentTime( double* current_time )
+{
+  if (!freq_initialized) {
+    LARGE_INTEGER freq;
+    use_high_res_timer = QueryPerformanceFrequency( &freq );
+    inv_freq = 1.0/freq.QuadPart;
+    freq_initialized = 1;
+  }
+  if (use_high_res_timer) {
+    LARGE_INTEGER c_time;
+    if (QueryPerformanceCounter( &c_time )) {
+      *current_time = c_time.QuadPart*inv_freq;
+    } else {
+      return false;
+    }
+  } else {
+    *current_time = ((double)timeGetTime( )) * 1.0e-3;
+  }
+  return true;
+}
+
+#else
+
+bool sutilCurrentTime( double* current_time )
+{
+  struct timeval tv;
+  if (gettimeofday( &tv, 0 )) {
+    fprintf( stderr, "sutilCurrentTime(): gettimeofday failed!\n" );
+    return false;
+  }
+
+  *current_time = tv.tv_sec+ tv.tv_usec * 1.0e-6;
+  return true;
+}
+
+#endif
 
 double Timer::start( )
 {
-  //sutilCurrentTime( &t0 ); return t0;
-  // TODO
-  t0 = 0.0; return t0;
+  sutilCurrentTime( &t0 ); return t0;
 }
 
 double Timer::stop( )
 {
-  //sutilCurrentTime( &t1 ); elapsed += t1-t0; return t1;
-  t1 = 0.0; elapsed += t1-t0; return t1;
+  sutilCurrentTime( &t1 ); elapsed += t1-t0; return t1;
 }
 
 void printTimeElapsed( Timer& t )
 {
-  t.stop();
+  if (t.t1 < t.t0) t.stop();
   std::cerr << std::setw( 8 ) 
             << std::fixed 
             << std::setprecision( 2 ) 
