904fde985f38d186dd6cf14f83a7835108c75826
Dylan Lacewell <lacewell@gmail.com>
2016-01-27 15:08:53 -0700

    handle missing ZLIB library when loading file

--- loaders/load_scene.cpp
+++ loaders/load_scene.cpp
@@ -19,10 +19,21 @@ bool load_scene( const char* filename, bake::Scene& scene, float* scene_bbox_min
   std::string extension = s.substr(pos);
   if (extension == ".obj") {
     return load_obj_scene(filename, scene, scene_bbox_min, scene_bbox_max, memory, num_instances_per_mesh);
-  } else if (extension == ".bk3d" || extension == ".gz") {
+  }
+  
+#ifdef NOGZLIB
+  if (extension == ".gz") {
+    std::cerr << "Unhandled .gz extension; must rebuild with ZLIB support to load this file." << std::endl;
+    std::cerr << filename << std::endl;
+    return false;
+  }
+#endif
+
+  if (extension == ".bk3d" || extension == ".gz") {
     return load_bk3d_scene(filename, scene, scene_bbox_min, scene_bbox_max, memory, num_instances_per_mesh);
   }
 
-  std::cerr << "Unrecognized filename extension: " << extension << std::endl;
-  return false;
+  std::cerr << "Unhandled filename extension: " << extension << ".  Attempting to load as bk3d" << std::endl;
+  std::cerr << filename << std::endl;
+  return load_bk3d_scene(filename, scene, scene_bbox_min, scene_bbox_max, memory, num_instances_per_mesh);
 }
